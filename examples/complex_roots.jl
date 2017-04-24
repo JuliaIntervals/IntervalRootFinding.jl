5a95d42d21dd338b4740ff2bdc3557a23d393ee6
David Sanders <dpsanders@gmail.com>
2017-04-24 01:06:41 -0500

    Add method for complex_bisection with two intervals instead of IntervalBox

--- examples/complex_roots.jl
+++ examples/complex_roots.jl
@@ -3,9 +3,26 @@
 
 using IntervalRootFinding, IntervalArithmetic
 
-# Find complex roots by bisection
+"""
+    complex_bisection(f, X)
+
+Find complex roots of $f: \mathbb{C} \to \mathbb{C}$.
+
+Inputs:
+
+- `f`: function that takes $z \in \mathbb{C}$ and returns another
+complex number.
+
+- `X`: An `IntervalBox` specifying the bounds on the real and imaginary parts
+of `z`.
+
+"""
 function complex_bisection(f, X::IntervalBox)
 
+    """
+    Make a 2D real version of the complex function `f` suitable for `bisection`,
+    i.e. that accepts an `IntervalBox` and returns an `IntervalBox`
+    """
     function g(X::IntervalBox)
         x, y = X
         z = x + im*y;
@@ -16,10 +33,18 @@ function complex_bisection(f, X::IntervalBox)
 
     roots = bisection(g, X)
 
-    return [root.interval[1] + im*root.interval[2] for root in roots]
+    return g, [root.interval[1] + im*root.interval[2] for root in roots]
 end
+
+"""
+    complex_bisection(f, x, y)
+
+Version in which the bounds are specified as two separate `Interval`s.
+"""
+complex_bisection(f, x::Interval, y::Interval) = complex_bisection(f, x × y)
 
 f(z) = z^4 + z - 2
 
-complex_bisection(f, (-100..100) × (-100..100) )
+L = 10
+g, roots = complex_bisection(f, -L..L, -L..L)
 
