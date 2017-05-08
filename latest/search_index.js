bed50beddf8ed5734545b8625b7f56741b0a9a2a
autodocs <autodocs>
2017-05-08 20:02:07 +0000

    build based on 6528e2f

--- latest/search_index.js
+++ latest/search_index.js
@@ -77,7 +77,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "IntervalRootFinding.bisect",
     "category": "Function",
-    "text": "bisect(X::IntervalBox, α=0.5)\n\nBisect the IntervalBox X at position α ∈ [0,1] along its longest side.\n\n\n\n"
+    "text": "bisect(X::Interval, α=0.5)\n\nSplit the interval X at position α; α=0.5 corresponds to the midpoint. Returns a tuple of the new intervals.\n\n\n\n"
 },
 
 {
@@ -85,7 +85,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "IntervalRootFinding.bisect",
     "category": "Function",
-    "text": "bisect(X::Interval, α=0.5)\n\nSplit the interval X at position α; α=0.5 corresponds to the midpoint. Returns a tuple of the new intervals.\n\n\n\n"
+    "text": "bisect(X::IntervalBox, α=0.5)\n\nBisect the IntervalBox X at position α ∈ [0,1] along its longest side.\n\n\n\n"
 },
 
 {
