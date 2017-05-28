c395d70712c70e17697fff3d6afe4d8369bd85d4
David Sanders <dpsanders@gmail.com>
2017-05-28 13:41:57 -0500

    Add bisection branch-and-prune example

--- examples/branch_and_prune.jl
+++ examples/branch_and_prune.jl
@@ -1,2 +1,5 @@
 branch_and_prune(-1e16..1e16, newton_helper(IntervalRootFinding.K, x->x^2-2)
+
 branch_and_prune(-1e16..1e16, newton_helper(IntervalRootFinding.N, x->x^2-2)
+
+branch_and_prune(-5..5, bisection_helper(sin), 1e-3)
