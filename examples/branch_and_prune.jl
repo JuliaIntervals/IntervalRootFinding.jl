a45c852a670f52e89e5413fbbfa4e050022c3921
David Sanders <dpsanders@gmail.com>
2017-05-28 22:02:04 -0500

    Add 2D bisection example

--- examples/branch_and_prune.jl
+++ examples/branch_and_prune.jl
@@ -2,3 +2,9 @@
 branch_and_prune(-5..5, sin, newton_helper(IntervalRootFinding.K))
 branch_and_prune(-5..5, sin, newton_helper(IntervalRootFinding.N))
 branch_and_prune(-5..5, sin, bisection_helper)
+
+# 2D:
+branch_and_prune(   (-5..5) × (-5..5),
+                    X -> ( (x,y) = X; IntervalBox(x^2 + y^2 - 1, y - x) ),
+                    bisection_helper
+                )
