f0a33ff4b6ad690ab735152593cc13df4faf3486
Eeshan Gupta <eeshan9815@gmail.com>
2018-05-18 10:20:52 +0530

    Display benchmark data in a table

--- perf/linear_eq.jl
+++ perf/linear_eq.jl
@@ -1,4 +1,4 @@
-using IntervalArithmetic, StaticArrays, BenchmarkTools, Compat
+using BenchmarkTools, Compat, DataFrames, IntervalRootFinding, IntervalArithmetic, StaticArrays
 
 function randVec(n::Int)
     a = randn(n)
@@ -17,18 +17,17 @@ function randMat(n::Int)
 end
 
 function benchmark(max=10)
+    df = DataFrame()
+    df[:Method] = ["Array", "MArray", "SArray1", "SArray2", "Contractor"]
     for n in 1:max
         A, mA, sA = randMat(n)
         b, mb, sb = randVec(n)
-        println("For n = ", n)
-        t1 =  @btime gauss_seidel_interval($A, $b)
-        println("Array: ", t1)
-        t2 =  @btime gauss_seidel_interval_static($mA, $mb)
-        println("MArray: ", t2)
-        t3 =  @btime gauss_seidel_interval_static1($sA, $sb)
-        println("SArray1: ", t3)
-        t4 =  @btime gauss_seidel_interval_static2($sA, $sb)
-        println("SArray2: ", t4)
-        println()
+        t1 = @belapsed gauss_seidel_interval($A, $b)
+        t2 = @belapsed gauss_seidel_interval_static($mA, $mb)
+        t3 = @belapsed gauss_seidel_interval_static1($sA, $sb)
+        t4 = @belapsed gauss_seidel_interval_static2($sA, $sb)
+        t5 = @belapsed gauss_seidel_contractor($A, $b)
+        df[Symbol("n = $n")] = [t1, t2, t3, t4, t5]
     end
+    println(df)
 end
