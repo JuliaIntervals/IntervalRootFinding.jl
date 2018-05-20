9d1aef210171a67efd6154af54a47d641563d0f7
Eeshan Gupta <eeshan9815@gmail.com>
2018-05-20 13:18:26 +0530

    Add tests and fix typos

--- src/IntervalRootFinding.jl
+++ src/IntervalRootFinding.jl
@@ -20,10 +20,8 @@ export
     roots, find_roots,
     bisect, newton1d, quadratic_roots,
     gauss_seidel_interval, gauss_seidel_interval!,
-    gauss_seidel_contractor, gauss_seidel_contractor!
-    # gauss_seidel_interval_static1, gauss_seidel_interval_static1!
-    # gauss_seidel_interval_static2, gauss_seidel_interval_static2!
-    # gauss_seidel_interval_static, gauss_seidel_interval_static!
+    gauss_seidel_contractor, gauss_seidel_contractor!,
+    gauss_elimination_interval, gauss_elimination_interval!
 
 export isunique, root_status
 
