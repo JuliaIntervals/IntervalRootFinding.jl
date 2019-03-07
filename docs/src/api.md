d45403de55e9672e04f93816db771c08d072b226
Benoît Richard <kolaru@hotmail.com>
2019-03-07 16:36:24 +0100

    Complete and correct docstrings
    Correct roots docstrings
    
    
    Correct BB docstrings

--- docs/src/api.md
+++ docs/src/api.md
@@ -1,6 +1,36 @@
 # API
 
+## Main interface
+
 ```@autodocs
 Modules = [IntervalRootFinding]
-Order   = [:type, :function]
+Pages   = ["roots.jl"]
+```
+
+## `Root` object
+
+```@autodocs
+Modules = [IntervalRootFinding]
+Pages   = ["root_object.jl"]
+```
+
+## Contractors
+
+```@autodocs
+Modules = [IntervalRootFinding]
+Pages   = ["contractors.jl"]
+```
+
+## Branch and bound search interface
+
+```@autodocs
+Modules = [IntervalRootFinding]
+Pages   = ["branch_and_bound.jl"]
+```
+
+## Others
+
+```@autodocs
+Modules = [IntervalRootFinding]
+Pages   = ["complex.jl", "linear_eq.jl", "newton1d.jl", "quadratic.jl", "slopes.jl"]
 ```
--- src/root_object.jl
+++ src/root_object.jl
@@ -1,22 +1,50 @@
 """
-Object representing a possible root inside a given `interval`.
-`status` is either `:unknown` or `:unique`.
-If `status` is `:unique` then we know that there is a unique root of the
-function in question inside the given `interval`.
+    Root
+
+Object representing a possible root inside a given region. The field `status`
+is either `:unknown` or `:unique`. If `status` is `:unique` then we know that
+there is a unique root of the function in question inside the given region.
+
+Internally the status may also be `:empty` for region guaranteed to contain no
+root, however such `Root`s are discarded by default and thus never returned
+by the `roots` function.
+
+# Fields
+  - `interval`: a region (either `Interval` of `IntervalBox`) searched for
+        roots.
+  - `status`: the status of the region, valid values are `:empty`, `unkown` and
+        `:unique`.
 """
 struct Root{T}
     interval::T
     status::Symbol
 end
 
-interval(x::Root) = x.interval
+interval(rt::Root) = rt.interval
 
-# use root_status since just `status` is too generic
-root_status(x::Root) = x.status
 
-show(io::IO, root::Root) = print(io, "Root($(root.interval), :$(root.status))")
+"""
+    root_status(rt)
 
-isunique(root::Root{T}) where {T} = (root.status == :unique)
+Return the status of a `Root`.
+"""
+root_status(rt::Root) = rt.status  # Use root_status since just `status` is too generic
+
+"""
+    root_region(rt)
+
+Return the region associated to a `Root`.
+"""
+root_region(rt::Root) = rt.interval  # More generic name than `interval`.
+
+"""
+    isunique(rt)
+
+Return whether a `Root` is unique.
+"""
+isunique(rt::Root{T}) where {T} = (rt.status == :unique)
+
+show(io::IO, rt::Root) = print(io, "Root($(rt.interval), :$(rt.status))")
 
 ⊆(a::Interval, b::Root) = a ⊆ b.interval   # the Root object has the interval in the first entry
 ⊆(a::Root, b::Root) = a.interval ⊆ b.interval
