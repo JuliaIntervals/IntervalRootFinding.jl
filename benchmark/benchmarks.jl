51aebdda8768a7c812396a03a1ac0824367e43eb
Benoît Richard <kolaru@hotmail.com>
2025-04-21 19:20:17 +0200

    Update benchmarks.jl

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,21 +5,22 @@ using BenchmarkTools
 using ForwardDiff
 using IntervalArithmetic
 using IntervalRootFinding
+using StaticArrays
 
 import Random
 
 const SUITE = BenchmarkGroup()
 
 Random.seed!(0)  # Seed the RNG to get consistent results
-tol = 1e-15
+tol = 1e-10
 
 include("dietmar_ratz_functions.jl")
 
 S = SUITE["Smiley"] = BenchmarkGroup()
 for example in (SmileyExample22, SmileyExample52, SmileyExample54) #, SmileyExample55)
     s = S[example.title] = BenchmarkGroup()
-    for method in (Newton, Krawczyk)
-        s[string(method)] = @benchmarkable roots($(example.f), $(example.region), $method, $tol)
+    for contractor in (Newton, Krawczyk)
+        s[string(contractor)] = @benchmarkable roots($(example.f), $(example.region) ; contractor = $contractor, abstol = $tol)
     end
 end
 
@@ -32,14 +33,13 @@ const A = 10
 f(x, y) = 2A + x^2 - A*cos(2π*x) + y^2 - A*cos(2π*y)
 f(X) = f(X...)
 
-ForwardDiff.gradient(f, X::IntervalBox) = ForwardDiff.gradient(f, X.v)
 ∇f = X -> ForwardDiff.gradient(f, X)
 
 L = 5.0
-X = IntervalBox(-L..(L+1), 2)
+X = SVector(interval(-L, (L+1)), interval(-L, (L+1)))
 
-for method in (Newton, Krawczyk)
-    S[string(method)] = @benchmarkable roots($(∇(f)), $X, $method, 1e-5)
+for contractor in (Newton, Krawczyk)
+    S[string(contractor)] = @benchmarkable roots($(∇f), $X ; contractor = $contractor, abstol = 1e-5)
 end
 
 
@@ -49,12 +49,12 @@ sizes = (2, 5, 10)
 
 for n in sizes
     s = S["n = $n"] = BenchmarkGroup()
-    A = interval.(randn(n, n))
+    M = interval.(randn(n, n))
     b = interval.(randn(n))
 
-    s["Gauss seidel"] = @benchmarkable gauss_seidel_interval($A, $b)
-    s["Gauss seidel contractor"] = @benchmarkable gauss_seidel_contractor($A, $b)
-    s["Gauss elimination"] = @benchmarkable gauss_elimination_interval($A, $b)
+    # s["Gauss seidel"] = @benchmarkable gauss_seidel_interval($M, $b)
+    # s["Gauss seidel contractor"] = @benchmarkable gauss_seidel_contractor($M, $b)
+    # s["Gauss elimination"] = @benchmarkable gauss_elimination_interval($M, $b)
 end
 
 
@@ -65,11 +65,8 @@ for (k, dr) in enumerate(dr_functions)
     s = S["Dietmar-Ratz $k"] = BenchmarkGroup()
 
     if k != 8  # dr8 is excluded as it has too many roots
-        for method in (Newton, Krawczyk)
-            s[string(method)] = @benchmarkable roots($dr, $X, $method, $tol)
+        for contractor in (Newton, Krawczyk)
+            s[string(contractor)] = @benchmarkable roots($dr, $X ; contractor = $contractor, abstol = $tol)
         end
     end
-
-    s["Automatic differentiation"] = @benchmarkable ForwardDiff.derivative($dr, $X)
-    s["Slope expansion"] = @benchmarkable slope($dr, $X, $(mid(X)))
 end
